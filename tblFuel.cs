3b83c0be7fa4936aabb498712b46f5c426d7903c
haelkh <elkhatibhassan42@gmail.com>
2024-07-21 22:59:56 +0300

    fuel form

---
+++ tblFuel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GasStationTemplate2
+{
+    public partial class Rate : Form
+    {
+        public Rate()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rate_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonHeader1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
